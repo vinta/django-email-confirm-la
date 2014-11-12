cad6581cb5522f607d1b9e3a43ae9574d36e2320
Vinta <vinta.chen@gmail.com>
2014-11-13 03:56:42 +0800

    admin action: resend_confirmation_email Fixes #5

--- email_confirm_la/admin.py
+++ email_confirm_la/admin.py
@@ -7,11 +7,21 @@ from django.contrib import admin
 from email_confirm_la.models import EmailConfirmation
 
 
+def resend_confirmation_email(modeladmin, request, queryset):
+    for confirmation in queryset:
+        if not confirmation.is_verified:
+            confirmation.send()
+
+resend_confirmation_email.short_description = 'Re-send selected %(verbose_name_plural)s'
+
+
 class EmailConfirmationAdmin(admin.ModelAdmin):
+
     def show_content_type(self, obj):
         return obj.content_object._meta.object_name
     show_content_type.short_description = 'Content type'
 
+    actions = [resend_confirmation_email, ]
     list_display = ('show_content_type', 'content_object', 'email_field_name', 'email', 'is_verified', 'is_primary', 'send_at', 'confirmed_at')
     list_display_links = list_display
     search_fields = ('email', )
