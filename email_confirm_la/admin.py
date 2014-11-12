a691b1dd9973e20dab98174e7d27b4592bb93056
Vinta <vinta.chen@gmail.com>
2014-11-13 00:53:07 +0800

    use _meta.object_name Fixes #13

--- email_confirm_la/admin.py
+++ email_confirm_la/admin.py
@@ -3,14 +3,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.contenttypes.models import ContentType
 
 from email_confirm_la.models import EmailConfirmation
 
 
 class EmailConfirmationAdmin(admin.ModelAdmin):
     def show_content_type(self, obj):
-        return ContentType.objects.get_for_model(obj).name
+        return obj.content_object._meta.object_name
     show_content_type.short_description = 'Content type'
 
     list_display = ('show_content_type', 'content_object', 'email_field_name', 'email', 'is_verified', 'is_primary', 'send_at', 'confirmed_at')
