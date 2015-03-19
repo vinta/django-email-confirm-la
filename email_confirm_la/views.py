d02a2ff7e4a5b5a11fe28222deddc1cfdc58516d
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2015-03-19 14:36:49 +0100

    Display the Email confirmation failure when there's either a wrong key, or an expired one

--- email_confirm_la/views.py
+++ email_confirm_la/views.py
@@ -6,16 +6,15 @@ from django.http import Http404
 from django.shortcuts import render
 
 from email_confirm_la.models import EmailConfirmation
+from email_confirm_la.exceptions import EmailConfirmationExpired
 
 
 def confirm_email(request, confirmation_key):
     try:
         email_confirmation = EmailConfirmation.objects.get(confirmation_key=confirmation_key)
-    except EmailConfirmation.DoesNotExist:
-        # TODO: show a email confirm fail page
-        raise Http404
-
-    email_confirmation.confirm()
+        email_confirmation.confirm()
+    except (EmailConfirmation.DoesNotExist, EmailConfirmationExpired):
+        return render(request, 'email_confirm_la/email_confirm_fail.html')
 
     context = {
         'email_confirmation': email_confirmation,
