c6ffea59cd6c1bbdf379ac05cbfd6e081219be95
Vinta <vinta.chen@gmail.com>
2016-07-25 17:06:24 +0800

    fix EmailConfirmationValidator

--- email_confirm_la/validators.py
+++ email_confirm_la/validators.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator
 from django.utils.translation import pgettext_lazy as _
@@ -14,18 +15,16 @@ class AuthUserEmailValidator(EmailValidator):
 
         from django.contrib.auth.models import User
 
-        email_exists = User.objects \
-            .filter(email__iexact=value) \
-            .exists()
-
-        if email_exists:
-            raise ValidationError(_(u'This email has already been used by someone else.'))
+        if User.objects.filter(email__iexact=value).exists():
+            raise ValidationError(_('ecla', 'This email has already been taken.'))
 
 
 class EmailConfirmationValidator(EmailValidator):
 
-    def __init__(self, content_type, email_field_name):
-        self.content_type = content_type
+    def __init__(self, content_object_model, email_field_name='email', regex=None, message=None, code=None):
+        super(EmailConfirmationValidator, self).__init__(regex, message, code)
+
+        self.content_type = ContentType.objects.get_for_model(content_object_model)
         self.email_field_name = email_field_name
 
     def __call__(self, value):
@@ -35,6 +34,5 @@ class EmailConfirmationValidator(EmailValidator):
             .filter(content_type=self.content_type, email_field_name=self.email_field_name) \
             .filter(email__iexact=value) \
             .exists()
-
         if email_exists:
-            raise ValidationError(_(u'This email has already been used by someone else.'))
+            raise ValidationError(_('ecla', 'This email has already been taken.'))
