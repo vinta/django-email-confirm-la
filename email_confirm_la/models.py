b9165b620eb1fec29089fd04eb5562564a6ec55c
Andrii Kostenko <andrey@kostenko.name>
2016-09-14 20:25:30 +0300

    Pass hostname to template

--- email_confirm_la/models.py
+++ email_confirm_la/models.py
@@ -36,16 +36,14 @@ class EmailConfirmationManager(models.Manager):
             confirmation.email_field_name = email_field_name
             confirmation.email = email
             confirmation.confirmation_key = confirmation_key
-            confirmation.hostname = hostname
             confirmation.save()
         except IntegrityError:
             confirmation = EmailConfirmation.objects.get_for_object(content_object, email_field_name)
             confirmation.email = email
             confirmation.confirmation_key = confirmation_key
-            confirmation.hostname = hostname
             confirmation.save(update_fields=['email', 'confirmation_key'])
 
-        confirmation.send()
+        confirmation.send({"hostname": hostname})
 
         return confirmation
 
