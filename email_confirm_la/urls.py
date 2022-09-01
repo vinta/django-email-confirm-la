39fb94833bea01e9861da91c21fe0e682e2fde6b
predatell <predatell@disroot.org>
2022-09-01 19:25:51 +0300

    support django 4

--- email_confirm_la/urls.py
+++ email_confirm_la/urls.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
-from django.conf.urls import url
+from django.urls import re_path
 from email_confirm_la.views import confirm_email
 
 
 urlpatterns = [
-    url(r'^key/(?P<confirmation_key>\w+)/$', confirm_email, name='confirm_email'),
+    re_path(r'^key/(?P<confirmation_key>\w+)/$', confirm_email, name='confirm_email'),
 ]
